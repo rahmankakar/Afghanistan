6efcb262508a4a4037e6b5403be8735090c604e3
rahmankakar <irfan.kakar10@gmail.com>
2022-11-10 10:30:15 -0800

    second commit

--- src/kunduz/Parwan.java
+++ src/kunduz/Parwan.java
@@ -4,7 +4,9 @@ public class Parwan {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("your to nice");
 	}
-
+public void second() {
+	System.out.println("ahmad,mahmoud,kalbir");
+}
 }
